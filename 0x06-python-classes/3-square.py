f54337b945afebb37a84904f46fbecdfcfa1d130
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-12-19 18:09:36 -0800

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define a square"""
+
+
+class Square:
+    """Represent a square"""
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Calculate the area of the square."""
+        return (self.__size ** 2)
