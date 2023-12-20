38613eb60443701495467bdac633420be565bd10
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-12-19 17:37:26 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    def __init__(self, size):
+        """Initialize.
+        Args:
+            size: The size of the new square.
+        """
+        self.__size = size
