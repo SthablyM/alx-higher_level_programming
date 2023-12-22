05bc345a086e137367189fef5734681b4a316eb9
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-12-22 00:44:59 -0800

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    """print the first x elements of a list that are intergers.
+    Args:
+        my_list (list): The  to print element from.
+        x (int): The number of element of my_list to print.
+    Returns:
+        number of element printed."""
+
+    count = 0
+    try:
+        for i in range(x):
+            value = my_list[i]
+            if type(value) == int:
+                print("{:d}".format(value), end="")
+                count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return count
