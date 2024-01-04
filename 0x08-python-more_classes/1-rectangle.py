bcd5e6c30aa3df40c11ca12007b390abe50ce6de
Mmathapelo Sebela <mathepelofentse@icloud.com>
2024-01-04 08:01:30 -0800

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
 
     @property
     def width(self):
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -29,7 +29,7 @@ class Rectangle:
 
     @property
     def height(self):
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
