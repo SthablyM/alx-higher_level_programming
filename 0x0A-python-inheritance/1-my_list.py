0a1eb98a425355888cc7c8da59d1dff78ea67627
Mmathapelo Sebela <mathepelofentse@icloud.com>
2024-01-08 10:00:37 -0800

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,5 +6,5 @@ class MyList(list):
     """Impliment the sorted printing"""
     def print_sorted(self):
         """ prints the list, but sorted (ascending sort)"""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        self.sort()
+        print(self)
