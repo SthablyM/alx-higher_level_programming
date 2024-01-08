df83304ccf8b7cd131ebbac4f829a86688a75b8a
Mmathapelo Sebela <mathepelofentse@icloud.com>
2024-01-08 06:23:53 -0800

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,4 +5,6 @@
 def inherits_from(obj, a_class):
     """check  if the object is an instance of a class that
     inherited (directly or indirectly) from the specified class"""
-    return issubclass(type(obj), a_class)
+    if type(obj) != a_class:
+        return True
+    return False
