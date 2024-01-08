f44ca41653b74a7a9504702ba5fe10630231b9dd
Mmathapelo Sebela <mathepelofentse@icloud.com>
2024-01-08 06:44:41 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Represent basegeometry class"""
+    def area(self):
+        """area() is not implemented"""
+        raise Exception("area() is not implemented")
