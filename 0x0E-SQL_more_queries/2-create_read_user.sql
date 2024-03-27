6c453417f79971b6bef05de18eb770068bbf9573
SthablyM <mathapelofentse@icloud.com>
2024-03-27 14:38:47 -0700

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,7 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2.
+-- create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- creates a user
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- grant select privileges to a user
 GRANT SELECT on hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
