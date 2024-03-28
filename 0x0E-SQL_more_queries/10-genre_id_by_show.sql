cc8b8ecfaa7aa90aa2ea9edec5d304f253cf8077
SthablyM <mathapelofentse@icloud.com>
2024-03-28 09:21:59 -0700

    tv shows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- dump from hbtn_0d_tvshows to your MySQL server: download
-SELECT tv_shows.tittle, tv_show_genres.genre_id 
+SELECT tv_shows.title, tv_show_genres.genre_id 
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
-ORDER BY tv_shows.tittle, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
