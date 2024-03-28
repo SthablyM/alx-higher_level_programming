6d9c39309e980e6a63142c5f9111be77aee557c2
SthablyM <mathapelofentse@icloud.com>
2024-03-28 10:17:00 -0700

    comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 --  lists all Comedy shows in the database hbtn_0d_tvshows.
 SELECT tv_shows.title
-FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+FROM tv_show_genres 
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_genres.name;
+ORDER BY tv_shows.title;
